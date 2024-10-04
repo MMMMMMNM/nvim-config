1ef10de015fc93ac9e69ba4c6afd5b0b81866c1e
MMMMMMNM <wwwwwwnm@outlook.com>
2024-10-04 20:58:57 +0800

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,8 +9,10 @@ return {
 		"folke/neodev.nvim",
 		"onsails/lspkind.nvim",
 		"nvimdev/lspsaga.nvim",
+		"rachartier/tiny-inline-diagnostic.nvim",
 	},
 	config = function()
+		vim.diagnostic.config({ virtual_text = false })
 		local servers = {
 			lua_ls = {
 				Lua = {
@@ -53,9 +55,105 @@ return {
 				vim.lsp.buf.format({ async = true })
 			end, "[F]ormat code")
 		end
+		require("tiny-inline-diagnostic").setup({
+			signs = {
+				left = "",
+				right = "",
+				diag = "●",
+				arrow = "    ",
+				up_arrow = "    ",
+				vertical = " │",
+				vertical_end = " └",
+			},
+			hi = {
+				error = "DiagnosticError",
+				warn = "DiagnosticWarn",
+				info = "DiagnosticInfo",
+				hint = "DiagnosticHint",
+				arrow = "NonText",
+				background = "CursorLine", -- Can be a highlight or a hexadecimal color (#RRGGBB)
+				mixing_color = "None", -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
+			},
+			blend = {
+				factor = 0.27,
+			},
+			options = {
+				-- Show the source of the diagnostic.
+				show_source = false,
+
+				-- Throttle the update of the diagnostic when moving cursor, in milliseconds.
+				-- You can increase it if you have performance issues.
+				-- Or set it to 0 to have better visuals.
+				throttle = 20,
+
+				-- The minimum length of the message, otherwise it will be on a new line.
+				softwrap = 15,
+
+				-- If multiple diagnostics are under the cursor, display all of them.
+				multiple_diag_under_cursor = false,
+
+				-- Enable diagnostic message on all lines.
+				multilines = false,
+
+				overflow = {
+					-- Manage the overflow of the message.
+					--    - wrap: when the message is too long, it is then displayed on multiple lines.
+					--    - none: the message will not be truncated.
+					--    - oneline: message will be displayed entirely on one line.
+					mode = "wrap",
+				},
+
+				-- Format the diagnostic message.
+				-- Example:
+				-- format = function(diagnostic)
+				--     return diagnostic.message .. " [" .. diagnostic.source .. "]"
+				-- end,
+				format = nil,
+
+				--- Enable it if you want to always have message with `after` characters length.
+				break_line = {
+					enabled = false,
+					after = 30,
+				},
+
+				virt_texts = {
+					priority = 2048,
+				},
+
+				-- Filter by severity.
+				severity = {
+					vim.diagnostic.severity.ERROR,
+					vim.diagnostic.severity.WARN,
+					vim.diagnostic.severity.INFO,
+					vim.diagnostic.severity.HINT,
+				},
+
+				-- Overwrite events to attach to a buffer. You should not change it, but if the plugin
+				-- does not works in your configuration, you may try to tweak it.
+				overwrite_events = nil,
+			},
+		})
 		require("lspkind").init({
+			-- DEPRECATED (use mode instead): enables text annotations
+			--
+			-- default: true
+			-- with_text = true,
+
+			-- defines how annotations are shown
+			-- default: symbol
+			-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
 			mode = "symbol_text",
+
+			-- default symbol map
+			-- can be either 'default' (requires nerd-fonts font) or
+			-- 'codicons' for codicon preset (requires vscode-codicons font)
+			--
+			-- default: 'default'
 			preset = "codicons",
+
+			-- override preset symbols
+			--
+			-- default: {}
 			symbol_map = {
 				Text = "󰉿",
 				Method = "󰆧",
