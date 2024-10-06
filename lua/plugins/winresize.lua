003b94cca2c16ecefaaa93442da09862b5f2a6c3
MMMMMMNM <wwwwwwnm@outlook.com>
2024-10-06 18:50:19 +0800

    ww

--- lua/plugins/winresize.lua
+++ lua/plugins/winresize.lua
@@ -1,12 +1,14 @@
-return{
-{
-    "pogyomo/winresize.nvim",
-    keys = {
-      { "<leader>rh", "<C-w><" },
-      { "<leader>rj", "<C-w>+" },
-      { "<leader>rk", "<C-w>-" },
-      { "<leader>rl", "<C-w>>" },
-    },
-    opts = {},
-  },
+return {
+	{
+		"pogyomo/winresize.nvim",
+		keys = {
+			{ "<leader>rh", "<C-w><" },
+			{ "<leader>rj", "<C-w>+" },
+			{ "<leader>rk", "<C-w>-" },
+			{ "<leader>rl", "<C-w>>" },
+		},
+		config = function()
+			require("winresize").setup()
+		end,
+	},
 }
